be788b7c1aa7aa54e6a4c80401424d02da8dd515
BAKHONGMIN <ghdals2655@naver.com>
2022-01-10 14:46:37 +0900

    js

---
+++ es6/db.js
@@ -0,0 +1,6 @@
+let naviText = ['네번째 그림', '작품들', '문의'];
+let naviLink = ['/company.html', '/work.html','/board.html'];
+
+let naviTag = '';
+
+naviTag = '<a href=\"'+naviLink[0]+'\">'+naviText[0]+'</a>';
